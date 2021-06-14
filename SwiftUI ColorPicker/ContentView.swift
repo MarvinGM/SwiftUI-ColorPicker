26a3c1a663c97e96e7ff7d08cca0dbaced73048e
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 19:05:06 +0800

    Initial Commit

---
+++ SwiftUI ColorPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI ColorPicker
+//
+//  Created by Amben on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI ColorPicker/SwiftUI_ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ColorPickerApp.swift
+//  SwiftUI ColorPicker
+//
+//  Created by Amben on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
