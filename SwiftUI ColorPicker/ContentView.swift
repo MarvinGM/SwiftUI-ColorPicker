9b3ae523ea36a21e1ef3107be86f236320fc2a29
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 19:17:32 +0800

    Version 1.0

--- SwiftUI ColorPicker/ContentView.swift
+++ SwiftUI ColorPicker/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var backgroundColor = Color(.systemBackground)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                backgroundColor
+                    //.ignoresSafeArea()
+                
+                ColorPicker("Select Color", selection: $backgroundColor)
+                    .padding()
+                
+            }
+            .navigationTitle("Color Picker")
+        }
     }
 }
 
